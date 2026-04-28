62492f534adb95b47f781b664f2bab83799a981e
Wajahat Ali Basharat <wajahat.ali.basharat@example.com>
2026-04-28 11:47:24 +0500

    Add tailwind configuration

---
+++ frontend/tailwind.config.ts
@@ -0,0 +1,26 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'hb-blue': '#0EA5E9',
+        'hb-green': '#22C55E', 
+        'hb-amber': '#F59E0B',
+        'hb-red': '#EF4444',
+      },
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
